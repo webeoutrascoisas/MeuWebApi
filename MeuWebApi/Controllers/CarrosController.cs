ae81286128544f0142f3abd5bd2cd532e0cf7e51
webeoutrascoisas <osiriskiller@gmail.com>
2014-06-02 09:59:21 -0300

    Controller implementada

--- MeuWebApi/Controllers/CarrosController.cs
+++ MeuWebApi/Controllers/CarrosController.cs
@@ -1,27 +1,80 @@
-﻿using System.Web.Http;
+﻿using System.Linq;
+using System.Web.Http.Results;
 using MeuWebApi.Models;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Http;
 
 namespace MeuWebApi.Controllers
 {
     public class CarrosController : ApiController
     {
+        public List<Carro> Carros
+        {
+            get
+            {
+                if (HttpContext.Current.Application["Carros"] == null)
+                {
+                    HttpContext.Current.Application["Carros"] = new List<Carro>
+                    {
+                        new Carro
+                        {
+                            Id = 1,
+                            Cor = "Prata",
+                            Nome = "Peugeot 160"
+                        },
+                        new Carro
+                        {
+                            Id = 2,
+                            Cor = "Branco",
+                            Nome = "JAC J3"
+                        }
+                    };
+
+                }
+
+                return (List<Carro>)HttpContext.Current.Application["Carros"];
+            }
+        }
+
         public IHttpActionResult Get()
         {
-            return null;
+            return Ok(Carros);
         }
 
         public IHttpActionResult Get(int id)
         {
-            return null;
+            var carro = Carros.FirstOrDefault(c => c.Id == id);
+
+            if (carro != null)
+                return Ok(carro);
+            return NotFound();
         }
 
         public IHttpActionResult Post(Carro carro)
         {
-            return null;
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            Carros.Add(carro);
+            return CreatedAtRoute("DefaultApi", new { id = carro.Id }, carro);
         }
+
         public IHttpActionResult Put(int id, [FromBody]Carro carro)
         {
-            return null;
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var car = Carros.FirstOrDefault(c => c.Id == id);
+
+            if (car != null)
+            {
+                car.Nome = carro.Nome;
+                car.Cor = carro.Cor;
+                return Ok();
+            }
+
+            return NotFound();
         }
     }
 }
--- MeuWebApi/Models/Carro.cs
+++ MeuWebApi/Models/Carro.cs
@@ -1,9 +1,16 @@
-﻿namespace MeuWebApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuWebApi.Models
 {
     public class Carro
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [Required]
         public string Cor { get; set; }
     }
 }
